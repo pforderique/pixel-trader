694441238b20eeda16c39e3c8de78d3417414506
pforderique <porderiq@mit.edu>
2022-01-17 23:53:24 -0500

    working explore page.

--- client/src/components/modules/UserBlock.js
+++ client/src/components/modules/UserBlock.js
@@ -1,16 +1,22 @@
 import React from "react";
 
+import noavatar from "../../public/noavatar.png";
 import "../../utilities.css";
 import "./UserBlock.css";
 
 const UserBlock = (props) => {
+  const onUserClick = () => {
+    window.location.href = `/profile/${props.user._id}`;
+  };
   return (
     <>
-      <div className="UserBlock-container">
+      <div className="UserBlock-container" onClick={onUserClick}>
         <div className="u-large">{props.user.name}</div>
-        <div className="UserBlock-headline1">
-          <div>{`following: ${props.user.following}`}</div>
+        <img className="UserBlock-img" src={noavatar} alt="profilepic" />
+        <div>{`networth: ${props.user.networth} VC`}</div>
+        <div className="UserBlock-headline2">
           <div>{`followers: ${props.user.followers}`}</div>
+          <div>{`following: ${props.user.following}`}</div>
         </div>
       </div>
     </>
